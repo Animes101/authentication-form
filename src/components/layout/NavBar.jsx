ce797ad8ba4912c1ad6da2f39dfcc7b36d616049
animes-barman <animesbarman101@gmail.com>
2024-03-31 01:28:22 +0600

    degign sing in or sing up button

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <div>
-      <header className="p-4 dark:bg-gray-100 dark:text-gray-800">
+      <header className="p-4 dark:bg-gray-100 bg-gray-400 dark:text-gray-800">
 	<div className="container flex justify-between h-16 mx-auto">
 		<a rel="noopener noreferrer" href="#" aria-label="Back to homepage" className="flex items-center p-2">
 			<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 32 32" className="w-8 h-8 dark:text-violet-600">
@@ -16,7 +16,7 @@ const NavBar = () => {
 				<a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border- dark:text-violet-600 dark:border-violet-600">Link</a>
 			</li>
 			<li className="flex">
-				<a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-">Link</a>
+				<a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-">login</a>
 			</li>
 			<li className="flex">
 				<a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-">Link</a>
@@ -26,8 +26,8 @@ const NavBar = () => {
 			</li>
 		</ul>
 		<div className="items-center flex-shrink-0 hidden lg:flex">
-			<button className="self-center px-8 py-3 rounded">Sign in</button>
-			<button className="self-center px-8 py-3 font-semibold rounded dark:bg-violet-600 dark:text-gray-50">Sign up</button>
+			<button className="self-center px-8 py-3 rounded"><NavLink to={`/signin`}>Sign in</NavLink></button>
+			<button className="self-center px-8 py-3 font-semibold rounded dark:bg-violet-600 dark:text-gray-50"><NavLink to={`/signup`}>Sign up</NavLink></button>
 		</div>
 		<button className="p-4 lg:hidden">
 			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 dark:text-gray-800">
@@ -36,7 +36,6 @@ const NavBar = () => {
 		</button>
 	</div>
 </header>
-    </div>
   )
 }
 
--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -2,11 +2,23 @@ import {
     createBrowserRouter,
   } from "react-router-dom";
 import MainLayout from "../components/layout/MainLayout";
+import SignIn from "../components/SignIn";
+import SignUp from "../components/SignUp";
 
 
 export  const router = createBrowserRouter([
     {
       path: "/",
       element: <MainLayout />,
+      children:[
+        {
+            path:'/signin',
+            element:<SignIn />
+        },
+        {
+            path:'/signup',
+            element:<SignUp />
+        }
+      ]
     },
   ]);
